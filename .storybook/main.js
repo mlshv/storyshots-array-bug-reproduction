62aaea7cdf1b0763aa1eb40e470d5caee6446282
Mikhail Malyshev <mmmm@yandex-team.ru>
2021-10-14 16:45:22 +0300

    Add reproduction

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,37 @@
+const path = require('path')
+const fs = require('fs')
+
+function findStories() {
+  function findStoriesPathsInDirectory(lookupPath) {
+    const stories = []
+
+    fs.readdirSync(lookupPath).forEach((fileName) => {
+      const filePath = lookupPath + '/' + fileName
+
+      const stats = fs.statSync(filePath)
+
+      if (stats.isDirectory()) {
+        stories.push(...findStoriesPathsInDirectory(filePath))
+      } else if (filePath.endsWith('.stories.tsx')) {
+        stories.push(filePath)
+      }
+    })
+
+    return stories
+  }
+
+  const rootPath = path.join(__dirname, '..', 'src')
+
+  return findStoriesPathsInDirectory(rootPath)
+}
+
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
+  "stories": findStories,
+  // uncomment default config to make it work again
+  // "stories": [
+  //   "../src/**/*.stories.mdx",
+  //   "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  // ],
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
